d1d003d0ece343f87cbc8edc1350f9f1e6953879
KeissyIparraguirre <74386629@institutocajas.info>
2024-10-08 13:22:26 -0500

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('surprise-btn').addEventListener('click', function() {
-    const surprise = document.getElementById('surprise');
-    surprise.style.display = surprise.style.display === 'none' ? 'block' : 'none';
-});
