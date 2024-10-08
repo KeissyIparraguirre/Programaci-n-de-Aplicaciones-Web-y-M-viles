0d4568364b322e6b9370d46f3b1aff9398586e8a
KeissyIparraguirre <74386629@institutocajas.info>
2024-10-08 13:22:44 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('surprise-btn').addEventListener('click', function() {
+    const surprise = document.getElementById('surprise');
+    surprise.style.display = surprise.style.display === 'none' ? 'block' : 'none';
+});
